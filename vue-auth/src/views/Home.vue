e8116a559c4eb29cb8d8ebaf2eac616107e3ae30
Dairo Portillo <dairo.jose17@hotmail.com>
2022-09-19 03:01:38 -0500

    Vue Commit

---
+++ vue-auth/src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="submit" class="row g-3">
+      <div class="col-auto">
+        <input v-model="data.nombre" type="text" class="form-control" id="nombre" placeholder="Nombre a buscar">
+      </div>
+      <div class="col-auto">
+        <input v-model="data.porcentaje" type="number" class="form-control" id="porcentaje" placeholder="porcentaje" step="0.01">
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Buscar</button>
+      </div>
+      <div class="col-auto mt-2">
+        <div class="alert alert-success" role="alert" v-if="data.statusOk">
+          Exitoso con resultados
+        </div>
+        <div class="alert alert-success" role="alert" v-if="data.statusEmpty">
+          Exitoso sin resultados
+        </div>
+      </div>
+    </form>
+    <div class="row">
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">Tipo Persona</th>
+          <th scope="col">Tipo Cargo</th>
+          <th scope="col">Departamento</th>
+          <th scope="col">Municipio</th>
+          <th scope="col">% Coincidencia</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(resultado, index) in data.resultados" :key="index">
+              <td>{{resultado.nombre}}</td>
+              <td>{{resultado.tipo_persona}}</td>
+              <td>{{resultado.tipo_cargo}}</td>
+              <td>{{resultado.departamento}}</td>
+              <td>{{resultado.municipio}}</td>
+              <td>{{resultado.ratio * 100}}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {onMounted, reactive, ref} from 'vue';
+import {useStore} from "vuex";
+import {useRouter} from "vue-router";
+
+export default {
+  name: "Home",
+  setup() {
+    const router = useRouter();
+
+    const data = reactive({
+      nombre: '',
+      porcentaje: '',
+      statusOk: false,
+      statusEmpty: false,
+      resultados: []
+    });
+
+    onMounted(async () => {
+
+      if (localStorage.getItem("token") == null || localStorage.token == "") await router.push('/login');
+
+    });
+
+    const submit = async () => {
+
+      data.statusEmpty = false
+      data.statusOk = false
+
+      try {
+        const response = await fetch('http://localhost:8000/api/personas/'
+            +"?nombre="+data.nombre+"&porcentaje="+data.porcentaje, {
+          headers: {'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+ localStorage.token},
+        });
+
+        if (response.status == 401) return alert("Sin Session")
+
+        const content = await response.json();
+
+        if (content.length == 0){
+          data.statusEmpty = true
+        }else{
+          data.statusOk = true
+        }
+
+        data.resultados = content
+
+
+      } catch (e) {
+        alert("Error servidor")
+      }
+    }
+
+    return {
+      data,
+      submit
+    }
+
+  }
+}
+</script>
---
+++ vue-auth/src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="submit">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <input v-model="data.username" type="text" class="form-control" placeholder="Email" required>
+
+    <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </form>
+</template>
+
+<script lang="ts">
+import {reactive} from 'vue';
+import {useRouter} from "vue-router";
+import {useStore} from "vuex";
+
+export default {
+  name: "Login",
+  setup() {
+    const data = reactive({
+      username: '',
+      password: ''
+    });
+    const router = useRouter();
+    const store = useStore();
+
+    const submit = async () => {
+      await fetch('http://localhost:8000/api/auth/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      }).then(async response => {
+        if (response.status == 400) return alert("Complete los campos")
+        if (response.status == 401) return alert("Credenciales inválidas")
+
+        const body = await response.json()
+        localStorage.token = body.access
+        store.state.authenticated = true
+
+      }).catch(error => {
+        alert(("Error de servidor"))
+      });
+
+      await router.push('/');
+    }
+
+    return {
+      data,
+      submit
+    }
+  }
+}
+</script>
